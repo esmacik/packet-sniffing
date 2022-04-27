e017e29e8e23404df664155bebae2ceba38c95bb
Erik Macik <42585523+esmacik@users.noreply.github.com>
2022-04-26 19:16:40 -0600

    Delete scapy_sniff.py

--- scapy_sniff.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-from scapy.all import *
-def print_pkt(pkt):
-    pkt.show()
-
-pkt = sniff(prn=print_pkt, filter="icmp")
-#pkt = sniff(prn=print_pkt, filter="tcp and src host 8.8.8.8 and dst port 8")
